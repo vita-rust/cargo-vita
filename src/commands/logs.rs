7db13a70e5eac1e49566781c93c9526ac2205eaa
Nikolay Arhipov <n@arhipov.net>
2023-11-02 11:48:11 +0200

    Updated README

--- src/commands/logs.rs
+++ src/commands/logs.rs
@@ -7,7 +7,7 @@ use std::{
 use anyhow::{bail, Context};
 use clap::{Args, Subcommand};
 use colored::Colorize;
-use log::{debug, error, info};
+use log::{debug, error, info, warn};
 
 use crate::ftp;
 
@@ -26,7 +26,7 @@ pub struct Logs {
 pub enum LogsCmd {
     /// Start a TCP server on 0.0.0.0 and print to stdout all bytes read from the socket
     Listen,
-    /// Reconfigures PrincessLog via vita-companion.
+    /// Reconfigures PrincessLog via vitacompanion.
     /// This will upload the configuration file with the ip address of your host and a port to your Vita.
     Configure(Configure),
 }
@@ -36,46 +36,107 @@ pub struct Configure {
     #[command(flatten)]
     pub connection: ConnectionArgs,
 
+    /// The IP address of the host to send logs to.
+    /// Vita will connect to this address via TCP write logs to the socket.
     #[arg(long)]
     host_ip_address: Option<String>,
 
+    /// Enabled kernel debug logs.
     #[arg(long, default_value = "false")]
     kernel_debug: bool,
 }
 
-static MAGIC: &[u8] = b"NLM\0";
+static MAGIC: [u8; 4] = [b'N', b'L', b'M', 0];
 static NLM_CONFIG_FLAGS_BIT_QAF_DEBUG_PRINTF: u32 = 1 << 0;
 
+struct PrincessLogConfig {
+    magic: [u8; 4],
+    ip: Ipv4Addr,
+    port: u16,
+    kernel_debug: bool,
+}
+
+impl PrincessLogConfig {
+    fn new(ip: Ipv4Addr, port: u16, kernel_debug: bool) -> Self {
+        Self {
+            magic: MAGIC,
+            ip,
+            port,
+            kernel_debug,
+        }
+    }
+
+    fn parse<R: Read>(config: &mut R) -> anyhow::Result<Self> {
+        let mut magic = [0; 4];
+        config.read_exact(&mut magic)?;
+
+        let mut buffer = [0; 4];
+        config.read_exact(&mut buffer)?;
+        let ip = Ipv4Addr::from(buffer);
+        config.read_exact(&mut buffer)?;
+        let flags = u32::from_le_bytes(buffer);
+        let mut buffer = [0; 2];
+        config.read_exact(&mut buffer)?;
+        let port = u16::from_le_bytes(buffer);
+
+        let kernel_debug = (flags & NLM_CONFIG_FLAGS_BIT_QAF_DEBUG_PRINTF) != 0;
+
+        Ok(Self {
+            magic,
+            ip,
+            port,
+            kernel_debug,
+        })
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        let flags = match self.kernel_debug {
+            true => NLM_CONFIG_FLAGS_BIT_QAF_DEBUG_PRINTF,
+            false => 0,
+        };
+
+        let mut res = Vec::with_capacity(16);
+        res.extend_from_slice(&MAGIC);
+        res.extend_from_slice(&self.ip.octets());
+        res.extend_from_slice(&flags.to_le_bytes());
+        res.extend_from_slice(&self.port.to_le_bytes());
+        res.extend_from_slice(&[0, 0]);
+
+        res
+    }
+}
+
 impl Logs {
-    fn configure(&self, configure: &Configure, verbose: u8) -> anyhow::Result<()> {
+    fn configure(&self, configure: &Configure) -> anyhow::Result<()> {
         let filename = "ur0:/data/NetLoggingMgrConfig.bin";
-        println!(
+        debug!(
             "{} {filename}",
             "Downloading the existing config from".blue()
         );
-        let mut ftp = ftp::connect(&configure.connection, verbose)?;
+        let mut ftp = ftp::connect(&configure.connection)?;
 
-        let file = ftp.retr_as_buffer(filename);
-        if let Ok(mut config) = file {
-            println!("{}", "Found existing config".blue());
-            let mut buffer = [0; 4];
-            config.read_exact(&mut buffer)?;
-            if buffer != MAGIC {
-                println!("{}", "Existing config is invalid".red());
+        match ftp.retr_as_buffer(filename) {
+            Ok(mut file) => {
+                info!("{}", "Found existing config".blue());
+
+                match PrincessLogConfig::parse(&mut file) {
+                    Ok(c) => match c.magic == MAGIC {
+                        true => info!(
+                            "{} {ip}:{port} {} {kdbg}",
+                            "Existing config has address".yellow(),
+                            "and kernel debug print is".yellow(),
+                            ip = c.ip,
+                            port = c.port,
+                            kdbg = c.kernel_debug
+                        ),
+                        false => warn!("{}", "Existing config has invalid magic".red()),
+                    },
+                    Err(err) => warn!("{}: {err}", "Failed to parse existing config".red()),
+                };
             }
-
-            config.read_exact(&mut buffer)?;
-            let ip = Ipv4Addr::from(buffer);
-            config.read_exact(&mut buffer)?;
-            let flags = u32::from_le_bytes(buffer);
-
-            let mut buffer = [0; 2];
-            config.read_exact(&mut buffer)?;
-            let port = u16::from_le_bytes(buffer);
-
-            println!("{}: {ip}:{port}", "Current log address".yellow());
-            let kdbg = (flags & NLM_CONFIG_FLAGS_BIT_QAF_DEBUG_PRINTF) != 0;
-            println!("{}: {kdbg}", "Kernel debug print".yellow());
+            Err(err) => {
+                warn!("{}: {err}", "Failed to download existing config".red());
+            }
         }
 
         let ip = match &configure.host_ip_address {
@@ -86,7 +147,7 @@ impl Logs {
             },
         };
 
-        println!(
+        info!(
             "{} {ip}:{port} {} {kdbg}",
             "Setting log address to".blue(),
             "and kernel debug print to".blue(),
@@ -94,31 +155,20 @@ impl Logs {
             kdbg = configure.kernel_debug,
         );
 
-        let mut config = Vec::new();
-        config.extend_from_slice(MAGIC);
-        config.extend_from_slice(&ip.octets());
-
-        let flags = match configure.kernel_debug {
-            true => NLM_CONFIG_FLAGS_BIT_QAF_DEBUG_PRINTF,
-            false => 0,
-        };
-
-        config.extend_from_slice(&flags.to_le_bytes());
-        config.extend_from_slice(&self.port.to_le_bytes());
-        config.extend_from_slice(&[0, 0]);
-
-        print!("{} {}", "Saving config to".blue(), filename);
+        info!("{} {}", "Saving config to".blue(), filename);
         let _ = ftp.mkdir("ur0:/data/");
-        ftp.put_file(filename, &mut Cursor::new(config))?;
+        let cfg = PrincessLogConfig::new(ip, self.port, configure.kernel_debug).serialize();
+        ftp.put_file(filename, &mut Cursor::new(cfg))?;
 
-        println!("Config will take effect after Vita is rebooted");
+        info!(
+            "{}",
+            "Config will take effect after Vita is rebooted".yellow()
+        );
 
         Ok(())
     }
-    fn listen(&self, verbose: u8) -> anyhow::Result<()> {
-        if verbose > 0 {
-            println!("{} {}", "Starting TCP server on port".blue(), self.port);
-        }
+    fn listen(&self) -> anyhow::Result<()> {
+        info!("{} {}", "Starting TCP server on port".blue(), self.port);
 
         let listener =
             TcpListener::bind(("0.0.0.0", self.port)).context("Unable to start TCP server")?;
@@ -162,10 +212,10 @@ impl Logs {
 }
 
 impl Executor for Logs {
-    fn execute(&self, verbose: u8) -> anyhow::Result<()> {
+    fn execute(&self) -> anyhow::Result<()> {
         match self.cmd.as_ref() {
-            Some(LogsCmd::Configure(cmd)) => self.configure(cmd, verbose),
-            Some(LogsCmd::Listen) | None => self.listen(verbose),
+            Some(LogsCmd::Configure(cmd)) => self.configure(cmd),
+            Some(LogsCmd::Listen) | None => self.listen(),
         }
     }
 }
