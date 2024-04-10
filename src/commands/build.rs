32f54b55e9e1291d7365d00422e294794e025d60
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2024-04-10 23:31:49 +0300

    fix: cargo uses debug folder for deb profile builds

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -320,7 +320,12 @@ impl<'a> BuildContext<'a> {
             .skip_while(|s| s.as_str() != "armv7-sony-vita-newlibeabihf")
             .nth(1);
 
-        let profile = profile.map_or("dev", |p| p.as_str());
+        let mut profile = profile.map_or("dev", |p| p.as_str());
+
+        // Cargo uses "debug" folder for "dev" profile builds
+        if profile == "debug" {
+            profile = "dev";
+        }
 
         if !art.meta.strip_symbols(profile) {
             info!("{}", "Skipping additional elf strip".yellow());
